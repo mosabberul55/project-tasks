629085d7a7d52d90900459e6954350740c338230
Mosabberul Islam <76517232+mosabberul55@users.noreply.github.com>
2020-12-31 23:07:26 +0600

    Update create.blade.php

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -13,19 +13,19 @@
       </div>
       <div class="form-group">
         <label for="high">High</label>
-        <input type="number" name="high" class="form-control" id="high">
+        <input type="text" name="high" class="form-control" id="high">
       </div>
       <div class="form-group">
         <label for="low">Low</label>
-        <input type="number" name="low" class="form-control" id="low">
+        <input type="text" name="low" class="form-control" id="low">
       </div>
       <div class="form-group">
         <label for="open">Open</label>
-        <input type="number" name="open" class="form-control" id="open">
+        <input type="text" name="open" class="form-control" id="open">
       </div>
       <div class="form-group">
         <label for="close">Close</label>
-        <input type="number" name="close" class="form-control" id="close">
+        <input type="text" name="close" class="form-control" id="close">
       </div>
       <div class="form-group">
         <label for="volume">Volume</label>
